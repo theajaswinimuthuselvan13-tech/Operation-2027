152b9e2eb0fd850d78680f80f626c7150fb53f7c
THEAJASWINI M <theajaswinimuthuselvan13@gmail.com>
2026-07-26 18:35:39 +0530

    First commit

---
+++ src/Tempcov01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Tempcov01 {
+public static void main( String[] args) {
+ Scanner sc= new Scanner(System.in);
+ System.out.println("enter temp");
+ float tempc= sc.nextFloat();
+ float tempf= (tempc*9/5) +32;
+System.out.println(tempf);
+}git --
+        }
