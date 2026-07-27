1f5d0488b5a15be4e044381be478bfff65328946
THEAJASWINI M <theajaswinimuthuselvan13@gmail.com>
2026-07-27 20:22:40 +0530

    calcprg

---
+++ src/Calc.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Calc {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+int ans=0;
+while(true){
+    System.out.println("Enter the operator");
+    char op =sc.next().trim().charAt(0);
+    if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+        System.out.println("Enter two numbers");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        if(op=='+'){
+     ans = n1+n2;
+        }
+            if(op=='-'){
+                 ans = n1-n2;
+            }
+                if(op=='*'){
+                     ans = n1*n2;
+                }
+                    if(op=='%'){
+                         ans = n1%n2;
+                    }
+                        if(op=='/'){if(n2!=0){
+                             ans = n1/n2;
+                        }
+
+                        }
+
+    }else if (op=='x'||op=='X'){
+        break;
+    }else{
+        System.out.println("invalid operation");
+    }
+
+System.out.println(ans);}
+    }
+}
