f6e12bf8abbd11b9e42058c982f13d50d39e888c
THEAJASWINI M <theajaswinimuthuselvan13@gmail.com>
2026-09-15 23:10:36 +0530

    Sum Of Two Num

---
+++ src/Sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Sum {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" ENTER FIRST NUMBER TO SUM ");
+        int a = sc.nextInt();
+        System.out.print(" ENTER Second NUMBER TO SUM ");
+        int b = sc.nextInt();
+        int s=a+b;
+        System.out.println("Sum of 2 number is "+s );
+
+    }
+}
