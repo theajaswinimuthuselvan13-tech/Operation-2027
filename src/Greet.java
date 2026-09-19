d1b1a8a22143bcfa22ca495469c3bb77e9d694b4
THEAJASWINI M <theajaswinimuthuselvan13@gmail.com>
2026-09-19 11:56:17 +0530

    func and methods

---
+++ src/Greet.java
@@ -0,0 +1,12 @@
+public class Greet {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.print(message);
+
+    }
+static String greet(){
+        String greeting = "Hiiii";
+        return greeting;
+}
+
+}
