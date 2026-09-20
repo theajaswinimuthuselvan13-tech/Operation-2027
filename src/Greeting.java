2c4dc91180c1cc6badf7148e2ac81cbddc51944a
THEAJASWINI M <theajaswinimuthuselvan13@gmail.com>
2026-09-21 00:01:20 +0530

    func and methods

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,12 +1,31 @@
-import java.util.*;
+//import java.util.*;
+//
+//public class Greeting {
+//public static void main (String[]args){
+//    String a= myGreet("Theaja");
+//    System.out.print(a);
+//}
+//static String myGreet( String name){
+//    String msg= "Hiiiiiiii "+name;
+//    return msg;
+//}
+//}
 
-public class Greeting {
-public static void main (String[]args){
-    String a= myGreet("Theaja");
-    System.out.print(a);
-}
-static String myGreet( String name){
-    String msg= "Hiiiiiiii "+name;
-    return msg;
-}
-}
+
+
+
+
+import java.util.*;
+public class Greeting{
+    public static void main(String[]args){
+    int a= 10;
+    int b=20;
+    swapp(a,b);
+       }
+    static void swapp(int x,int y){
+        int temp=x;
+        x=y;
+        y=temp;
+        System.out.println(x+" "+y);;
+        }
+    }
