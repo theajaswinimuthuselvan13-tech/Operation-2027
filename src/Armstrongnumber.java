508c992f7b779fc03277cb9ffb54130d69d9b0c4
THEAJASWINI M <theajaswinimuthuselvan13@gmail.com>
2026-09-21 21:17:29 +0530

    3-digit armstrong num

---
+++ src/Armstrongnumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Armstrongnumber {
+  public   static void main(String[] args) {
+      Scanner sc= new Scanner(System.in);
+      int n =sc.nextInt();
+      System.out.println(armstrong(n));
+
+    }
+    static boolean armstrong(int num){
+      int sum=0;
+      int original=num;
+      while(num>0){
+          int rem = num%10;
+          int cube= rem*rem*rem;
+          sum+=cube;
+          num/=10;
+      } return original==sum;
+    }
+}
